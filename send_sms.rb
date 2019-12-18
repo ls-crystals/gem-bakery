90517a2fde00fbd5b6459ac99fe07b4a9644f3a5
crystal <crystalrobertson236@gmail.com>
2019-12-17 23:49:35 -0500

    final commit

--- send_sms.rb
+++ send_sms.rb
@@ -1,16 +1,14 @@
-require("bundler")
-Bundler.require
-
-
 
 require 'twilio-ruby'
 
-account_sid = 'ACafb5285b10d86e41197d48b1d4bc708b'
-auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyy'
+account_sid = 'TWILIO_ACCOUNT_SID'
+auth_token = 'youraccountsid'
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
-from = '+15551234567' # Your Twilio number
-to = '+15555555555' # Your mobile phone number
+from = '5035361872'
+
+ # Your Twilio number
+to = '7189027499' # Your mobile phone number
 
 client.messages.create(
 from: from,
