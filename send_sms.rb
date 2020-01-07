1597d5c2fd2efa7d8ae30461021310c1e0caac1a
crystal <crystalrobertson236@gmail.com>
2020-01-06 19:32:01 -0500

    security update

--- send_sms.rb
+++ send_sms.rb
@@ -5,10 +5,10 @@ account_sid = 'TWILIO_ACCOUNT_SID'
 auth_token = 'youraccountsid'
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
-from = '5035361872'
+from = 'FROM_NUM'
 
  # Your Twilio number
-to = '7189027499' # Your mobile phone number
+to = 'TO_NUM' # Your mobile phone number
 
 client.messages.create(
 from: from,
